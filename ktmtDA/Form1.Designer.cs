2e2d5a073512a82c1b175d84973cfdaa503c4a50
Phạm Hoài Lâm <phamlam090@gmail.com>
2018-05-17 22:26:21 +0700

    DoAn1KTMT

---
+++ ktmtDA/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ktmtDA
+{
+    partial class ImageShow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.btnLoadImage);
+            this.panel1.Location = new System.Drawing.Point(575, 294);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(286, 188);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(77, 1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.MouseHover += new System.EventHandler(this.textBox1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(273, 121);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.MouseHover += new System.EventHandler(this.listBox1_MouseHover);
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(6, 151);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadImage.TabIndex = 0;
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            this.btnLoadImage.MouseHover += new System.EventHandler(this.btnLoadImage_MouseHover);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(569, 481);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(70, 70);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(575, 30);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(282, 258);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(578, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            this.label2.MouseHover += new System.EventHandler(this.label2_MouseHover);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 3000;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // toolTip2
+            // 
+            this.toolTip2.AutoPopDelay = 10000;
+            this.toolTip2.InitialDelay = 500;
+            this.toolTip2.ReshowDelay = 100;
+            this.toolTip2.ShowAlways = true;
+            // 
+            // ImageShow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(863, 504);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "ImageShow";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ImageShow_FormClosing);
+            this.Load += new System.EventHandler(this.ImageShow_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+    }
+}
+
---
+++ ktmtDA/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace ktmtDA
+{
+    public partial class ImageShow : Form
+    {
+        string Dirpath;//bien lay duong dan thu muc
+        StringCollection stringCol = new StringCollection();//luu lich su xem
+
+        int ii= Properties.Settings.Default.History.Count;//bien index trong listbox
+       
+        int i=0;//bien dem trong listbox va listview
+        int iold, inew;//dem so luong anh trong thu muc
+        public ImageShow()
+        {
+            InitializeComponent();
+            btnLoadImage.Text = Resource1.LoadImage;// Dat ten cho nut tai hinh anh
+            Text = Resource1.ImageShow;//Dat ten cho chuong trinh
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            DialogResult mess = folderBrowserDialog1.ShowDialog();
+            iold = i;
+
+            if (mess == DialogResult.OK)
+            {
+                listView1.Items.Clear();//Lam moi listview
+                Dirpath = folderBrowserDialog1.SelectedPath;
+                label2.Text = Dirpath;//Hien thi duong dan thu muc
+ 
+                pictureBox1.Image = null;
+                string[] Files = Directory.GetFiles(Dirpath);                            
+                foreach (String fn in Files)//lay cac duong dan anh
+                {                  
+                    if (fn.ToLower().EndsWith(".jpg") || fn.ToLower().EndsWith(".gif") ||
+                    fn.ToLower().EndsWith(".png") || fn.ToLower().EndsWith(".bmp") ||
+                    fn.ToLower().EndsWith(".jpeg"))
+                    {
+                        ListViewItem item = new ListViewItem();//them cac item anh vao listview
+                        Image pic;
+                        pic = Image.FromFile(fn);
+                        imageList1.Images.Add(pic);
+                        item.Name = fn;
+                        item.ImageIndex = i;
+                        listView1.Items.Add(item);                        
+                        i++;
+                    }
+                }
+            }
+            inew = i;
+
+        }
+        
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Resource1.History;
+            string NameImage = listView1.SelectedItems[0].Name;
+            string NameAndTime;//lay ngay gio he thong
+
+            int day = DateTime.Now.Day;
+            string sDay;
+            if (day < 10)
+                sDay = "0" + day.ToString();
+            else sDay = day.ToString();
+
+            int month = DateTime.Now.Month;
+            string sMonth;
+            if (month < 10)
+                sMonth = "0" + month.ToString();
+            else sMonth = month.ToString();
+
+            int year = DateTime.Now.Year;
+
+            int hour = DateTime.Now.Hour;
+            string sHour;
+            if (hour < 10)
+                sHour = "0" + hour.ToString();
+            else sHour = hour.ToString();
+
+            int minute = DateTime.Now.Minute;
+            string sMinute;
+            if (minute < 10)
+                sMinute = "0" + minute.ToString();
+            else sMinute = minute.ToString();
+
+            int second = DateTime.Now.Second;
+            string sSecond;
+            if (second < 10)
+                sSecond = "0" + second.ToString();
+            else sSecond = second.ToString();
+
+            NameAndTime = sHour + ":" + sMinute + ":" + sSecond + "_" + sDay +"/"+ sMonth +"/"+ year.ToString()+" "+NameImage;
+            //ten item gom thoi gian he thong va duong dan anh
+            listBox1.Items.Add(NameAndTime);//them item vao listbox
+
+            stringCol.Add(NameAndTime);//luu lich su
+            
+            listBox1.SelectedIndex =ii;//tro den item moi dc them vao
+            ii++;
+
+            pictureBox1.Image = Image.FromFile(NameImage);//hien thi anh vao picturebox
+            textBox1.Text = NameImage;//dua duong dan ra textbox
+            textBox1.Enabled = true;
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            string ListBoxItemName = listBox1.SelectedItem.ToString();
+            int strindex = 0;//loai bo ngay gio trong cac item cua listbox
+            char temp = '.';
+            while (temp != ' ')
+            {
+                temp = ListBoxItemName[strindex];
+                strindex++;
+            }
+            string lbin = ListBoxItemName.Remove(0, strindex - 1);
+            textBox1.Text = lbin;//dua duong dan ra textbox
+            textBox1.Enabled = true;
+            pictureBox1.ImageLocation = lbin;//hien thi anh khi click vao listbox
+            
+        }
+
+        private void ImageShow_Load(object sender, EventArgs e)
+        {
+            Dirpath = Properties.Settings.Default.RecentFolder;
+            label2.Text = Dirpath;
+            string[] Files = Directory.GetFiles(Dirpath);
+            
+            foreach (String fn in Files)//lay cac duong dan anh
+            {
+                if (fn.ToLower().EndsWith(".jpg") || fn.ToLower().EndsWith(".gif") ||
+                fn.ToLower().EndsWith(".png") || fn.ToLower().EndsWith(".bmp") ||
+                fn.ToLower().EndsWith(".jpeg"))
+                {
+                    ListViewItem item = new ListViewItem();//them cac item anh vao listview
+                    Image pic;
+                    pic = Image.FromFile(fn);
+                    imageList1.Images.Add(pic);
+                    item.Name = fn;
+                    item.ImageIndex = i;
+                    listView1.Items.Add(item);
+                    i++;
+                }
+            }
+            inew = i; iold = 0;
+            for (int j=0;j<Properties.Settings.Default.History.Count;j++)//tai lai lich su xem luc truoc
+            {
+                listBox1.Items.Add(Properties.Settings.Default.History[j]);
+            }
+            listBox1.SelectedIndex = Properties.Settings.Default.History.Count - 1;//chi den hinh anh dc xem gan nhat
+
+            if(listBox1.Items.Count>0)
+            {
+                label1.Text = Resource1.History;   
+                string ListBoxItemName = listBox1.SelectedItem.ToString();
+                int strindex = 0;//loai bo ngay gio trong cac item cua listbox
+                char temp = '.';
+                while (temp != ' ')
+                {
+                    temp = ListBoxItemName[strindex];
+                    strindex++;
+                }
+                string lbin = ListBoxItemName.Remove(0, strindex - 1);
+                textBox1.Text = lbin;//dua duong dan ra textbox
+                textBox1.Enabled = true;
+                pictureBox1.ImageLocation = lbin;//hien thi anh
+            }
+            
+        }
+
+
+        private void ImageShow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.RecentFolder = Dirpath;//luu lai duong dan thu muc truoc do
+            Properties.Settings.Default.History = stringCol;//luu lai lich su xem
+            Properties.Settings.Default.Save();
+        }
+
+
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            string Info = "";//thong tin tooltip cua anh
+            if (pictureBox1.Image != null)
+            {
+                FileInfo FI = new FileInfo(textBox1.Text.ToString());
+                Info =  Resource1.Name_Image + ": " + FI.Name;
+                Info = Info + "\n" + Resource1.Dimensions_Image+": " + pictureBox1.Image.Width.ToString()+ "x" +pictureBox1.Image.Height.ToString();
+                float lenImage = (float)FI.Length / 1024;
+                Info = Info + "\n" + Resource1.Size_Image + ": " + lenImage + "Kb";
+                Info = Info + "\n" + Resource1.CreationTime_Image + ": " + FI.CreationTime;
+                toolTip2.SetToolTip(pictureBox1, Info);
+            }
+            else
+            {
+                toolTip2.SetToolTip(pictureBox1, Resource1.PicBoxToolTip);
+            }
+        }
+
+        
+
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            if(label2.Text!=null)
+            {
+                toolTip1.ToolTipTitle=Dirpath;
+                if ((inew-iold) == 0)
+                {
+                    toolTip1.SetToolTip(label2, "0 image");
+                }
+                else if((inew - iold)== 1)
+                {
+                    toolTip1.SetToolTip(label2, "1 "+Resource1.Image);
+                }
+                else
+                {
+                    toolTip1.SetToolTip(label2,inew - iold + " " + Resource1.Images);
+                }
+            }
+        }
+
+        private void btnLoadImage_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.ToolTipTitle = Resource1.LoadImage;//Title cua toolTip
+            toolTip1.SetToolTip(btnLoadImage, Resource1.LoadImageToolTip);//Hien thi toolTip cho nut LoadImage  
+        }
+
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.ToolTipTitle = textBox1.Text;
+            toolTip1.SetToolTip(textBox1, textBox1.Text);
+        }
+
+        private void listBox1_MouseHover(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index != -1 && index < listBox1.Items.Count)
+            {
+                toolTip3.SetToolTip(listBox1, listBox1.Items[index].ToString());
+            }
+        }
+
+       
+    }
+}
